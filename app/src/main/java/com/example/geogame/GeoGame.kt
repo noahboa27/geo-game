4d024b39ce3ffaffe15223f2d5c519e5225abac4
noahboa <noah.whited@protonmail.com>
2023-03-04 18:28:44 -0700

    Setting up db instance

--- app/src/main/java/com/example/geogame/GeoGame.kt
+++ app/src/main/java/com/example/geogame/GeoGame.kt
@@ -3,5 +3,9 @@ package com.example.geogame
 import android.app.Application
 
 class GeoGame : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        // Probably get the countries here?
+        // Create db and store them
+    }
 }
--- app/src/main/java/com/example/geogame/core/database/AppDatabase.kt
+++ app/src/main/java/com/example/geogame/core/database/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.geogame.core.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.geogame.core.data.dao.CountriesDao
 import com.example.geogame.core.data.model.Country
@@ -11,4 +13,19 @@ import com.example.geogame.core.data.model.Country
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun countryDao(): CountriesDao
+
+    companion object {
+        private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase =
+            instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build().also {
+                    instance = it
+                }
+            }
+    }
 }
