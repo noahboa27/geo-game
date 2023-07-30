b84fc8328bcd1326c1a5290d5a75667de69df6a5
noahboa <noah.whited@protonmail.com>
2023-07-30 16:27:39 -0600

    Updated UI. Next step is to make it stateless.

--- app/src/main/java/com/example/geogame/flag_game/presentation/FlagGameContent.kt
+++ app/src/main/java/com/example/geogame/flag_game/presentation/FlagGameContent.kt
@@ -1,8 +1,14 @@
 package com.example.geogame.flag_game.presentation
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -10,63 +16,114 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.geogame.R
 
 @Composable
 fun FlagGameContent() {
-    Column {
+    ConstraintLayout(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val (quitButton, flagImage, answerOptionBox, progressBar, progressText) = createRefs()
+        val flagImageGuideline = createGuidelineFromTop(0.2f)
+        val answerBoxGuideline = createGuidelineFromTop(0.65f)
+        val progressBarGuideline = createGuidelineFromBottom(0.07f)
+
         Button(
-            modifier = Modifier.align(Alignment.End),
+            shape = RoundedCornerShape(percent = 50),
+            modifier = Modifier
+                .constrainAs(quitButton) {
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                }
+                .padding(10.dp),
             onClick = { /*TODO*/ }
         ) {
             Text(text = "Quit")
         }
 
-        Spacer(modifier = Modifier.height(10.dp))
-
         Image(
             painter = painterResource(id = R.drawable.united_states_flag),
-            contentDescription = "US Flag"
+            contentDescription = "US Flag",
+            modifier = Modifier
+                .constrainAs(flagImage) {
+                    top.linkTo(flagImageGuideline)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
         )
-
-        Spacer(modifier = Modifier.height(10.dp))
 
         Box(
             modifier = Modifier
-                .fillMaxHeight(0.18f)
-                .fillMaxWidth(0.5f),
+                .fillMaxHeight(0.14f)
+                .fillMaxWidth(0.75f)
+                .constrainAs(answerOptionBox) {
+                    top.linkTo(answerBoxGuideline)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
             contentAlignment = Alignment.Center
         ) {
             Button(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.align(Alignment.TopStart)
+                shape = RoundedCornerShape(percent = 50),
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .fillMaxWidth(0.45f),
+                onClick = { /*TODO*/ }
             ) {
                 Text(text = "Option 1")
             }
 
             Button(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.align(Alignment.TopEnd)
+                shape = RoundedCornerShape(percent = 50),
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .fillMaxWidth(0.45f),
+                onClick = { /*TODO*/ }
             ) {
                 Text(text = "Option 2")
             }
 
             Button(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.align(Alignment.BottomStart)
+                shape = RoundedCornerShape(percent = 50),
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .fillMaxWidth(0.45f),
+                onClick = { /*TODO*/ }
             ) {
                 Text(text = "Option 3")
             }
 
             Button(
-                onClick = { /*TODO*/ },
-                modifier = Modifier.align(Alignment.BottomEnd)
+                shape = RoundedCornerShape(percent = 50),
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .fillMaxWidth(0.45f),
+                onClick = { /*TODO*/ }
             ) {
                 Text(text = "Option 4")
             }
         }
 
-        Spacer(modifier = Modifier.height(10.dp))
+        LinearProgressIndicator(
+            progress = 0.4f,
+            modifier = Modifier
+                .constrainAs(progressBar) {
+                    top.linkTo(progressBarGuideline)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        )
+
+        Text(
+            text = "4 / 10",
+            modifier = Modifier
+                .constrainAs(progressText) {
+                    top.linkTo(progressBar.bottom, margin = 10.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        )
     }
 }
 
