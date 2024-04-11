f77e2c26545f9a5e2e9b31db6c3b59cbb318e5c4
noahboa <noah.whited@protonmail.com>
2024-04-11 13:23:15 -0600

    unused imports

--- app/src/main/java/com/example/geogame/main_menu/ui/MainMenuContent.kt
+++ app/src/main/java/com/example/geogame/main_menu/ui/MainMenuContent.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
