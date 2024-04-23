8e66bffcbe7ce79e1e0a2e7f0e80a9a3d5fdb0f3
noahboa27 <noah.whited@protonmail.com>
2024-04-23 14:51:54 -0600

    removed this in favor of a data class for the state

--- app/src/main/java/com/example/geogame/main_menu/state/MainMenuState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.geogame.main_menu.state
-
-sealed class MainMenuState {
-    object Initial : MainMenuState()
-    object Loading : MainMenuState()
-    data class Error(val errorMessage: String) : MainMenuState()
-    object StartFlagGame : MainMenuState()
-}
