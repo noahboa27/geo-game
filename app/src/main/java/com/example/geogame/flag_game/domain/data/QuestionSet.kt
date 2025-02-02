a0613f9a370773704da056d2a4a7d72be0057444
noahboa27 <noah.whited@protonmail.com>
2025-02-02 16:05:50 -0700

    simplified the game logic so these aren't needed anymore

--- app/src/main/java/com/example/geogame/flag_game/domain/data/QuestionSet.kt
+++
@@ -1,9 +0,0 @@
-package com.example.geogame.flag_game.domain.data
-
-import com.example.geogame.core.domain.model.FlagGameCountry
-import com.example.geogame.core.domain.model.Name
-
-data class QuestionSet(
-    val options: List<FlagGameCountry> = listOf(),
-    val answer: FlagGameCountry = FlagGameCountry(flagUrl = "", name = Name())
-)
