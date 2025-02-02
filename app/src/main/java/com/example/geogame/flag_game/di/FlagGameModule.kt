09adfbf5482fea16d4d22efa2b3170540d61ea13
noahboa27 <noah.whited@protonmail.com>
2025-02-02 16:18:03 -0700

    unused imports

--- app/src/main/java/com/example/geogame/flag_game/di/FlagGameModule.kt
+++ app/src/main/java/com/example/geogame/flag_game/di/FlagGameModule.kt
@@ -1,11 +1,7 @@
 package com.example.geogame.flag_game.di
 
-import coil3.ImageLoader
-import coil3.SingletonImageLoader
-import coil3.request.ImageRequest
 import com.example.geogame.flag_game.domain.useCase.GetRandomCountriesUseCase
 import com.example.geogame.flag_game.presentation.viewModel.FlagGameViewModel
-import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
