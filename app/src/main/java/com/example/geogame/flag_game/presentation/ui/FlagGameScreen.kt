b873b80a55dc7172ebb042487283d7b89c37e000
noahboa27 <noah.whited@protonmail.com>
2025-02-04 21:12:04 -0700

    got rid of constraints in the ui

--- app/src/main/java/com/example/geogame/flag_game/presentation/ui/FlagGameScreen.kt
+++ app/src/main/java/com/example/geogame/flag_game/presentation/ui/FlagGameScreen.kt
@@ -1,6 +1,8 @@
 package com.example.geogame.flag_game.presentation.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,14 +15,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
-import com.example.geogame.R
 import com.example.geogame.flag_game.domain.data.FlagGameCountry
 import com.example.geogame.flag_game.presentation.viewModel.FlagGameViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -38,7 +38,7 @@ fun FlagGameScreen(
         }
 
         val currentQuestion = state.value.currentQuestion
-        val answer = currentQuestion[state.value.answerIndex]
+        val answer = state.value.currentAnswer
 
         FlagGameContent(
             isLoading = state.value.isLoading,
@@ -67,23 +67,14 @@ fun FlagGameContent(
     onAnswerClicked: (Boolean) -> Unit
 ) {
 
-    ConstraintLayout(
+    Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        //FIXME: remove constraints and use spacers with percentages
-        val (quitButton, flagImage, answerOptionBox, progressBar, progressText) = createRefs()
-        val flagImageGuideline = createGuidelineFromTop(0.2f)
-        val answerBoxGuideline = createGuidelineFromTop(0.65f)
-        val progressBarGuideline = createGuidelineFromBottom(0.07f)
-
         Button(
             shape = RoundedCornerShape(percent = 50),
             modifier = Modifier
-                .constrainAs(quitButton) {
-                    top.linkTo(parent.top)
-                    end.linkTo(parent.end)
-                }
-                .padding(10.dp),
+                .padding(10.dp)
+                .align(Alignment.End),
             onClick = { onQuitClicked() }
         ) {
             Text(text = "Quit")
@@ -94,14 +85,10 @@ fun FlagGameContent(
                 .data(flagUrl)
                 .build(),
             contentDescription = "Country Flag",
-            placeholder = painterResource(R.drawable.loading_stub),
-            onSuccess = {  },
             modifier = Modifier
-                .constrainAs(flagImage) {
-                    top.linkTo(flagImageGuideline)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
+                .fillMaxWidth(0.9f)
+                .weight(1f)
+                .align(Alignment.CenterHorizontally)
         )
 //        Image(
 //            //FIXME don't use fixed drawable
@@ -117,87 +104,96 @@ fun FlagGameContent(
 
         Box(
             modifier = Modifier
-                .fillMaxHeight(0.14f)
-                .fillMaxWidth(0.75f)
-                .constrainAs(answerOptionBox) {
-                    top.linkTo(answerBoxGuideline)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                },
+                .fillMaxHeight(0.25f)
+                .fillMaxWidth(0.9f)
+                .align(Alignment.CenterHorizontally),
             contentAlignment = Alignment.Center
         ) {
             // top left
             Button(
-                shape = RoundedCornerShape(percent = 50),
+                shape = RoundedCornerShape(percent = 20),
                 modifier = Modifier
-                    .align(Alignment.TopStart)
-                    .fillMaxWidth(0.45f),
+                    .fillMaxHeight(0.48f)
+                    .fillMaxWidth(0.48f)
+                    .align(Alignment.TopStart),
                 onClick = {
                     onAnswerClicked(currentQuestion[0].isAnswer)
                 }
             ) {
-                Text(text = currentQuestion[0].name)
+                Text(
+                    text = currentQuestion[0].name,
+                    textAlign = TextAlign.Center
+                )
             }
 
             // top right
             Button(
-                shape = RoundedCornerShape(percent = 50),
+                shape = RoundedCornerShape(percent = 20),
                 modifier = Modifier
-                    .align(Alignment.TopEnd)
-                    .fillMaxWidth(0.45f),
+                    .fillMaxHeight(0.48f)
+                    .fillMaxWidth(0.48f)
+                    .align(Alignment.TopEnd),
                 onClick = {
                     onAnswerClicked(currentQuestion[1].isAnswer)
                 }
             ) {
-                Text(text = currentQuestion[1].name)
+                Text(
+                    text = currentQuestion[1].name,
+                    textAlign = TextAlign.Center
+                )
             }
 
             // bottom left
             Button(
-                shape = RoundedCornerShape(percent = 50),
+                shape = RoundedCornerShape(percent = 20),
                 modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .fillMaxWidth(0.45f),
+                    .fillMaxHeight(0.48f)
+                    .fillMaxWidth(0.48f)
+                    .align(Alignment.BottomStart),
                 onClick = {
                     onAnswerClicked(currentQuestion[2].isAnswer)
                 }
             ) {
-                Text(text = currentQuestion[2].name)
+                Text(
+                    text = currentQuestion[2].name,
+                    textAlign = TextAlign.Center
+                )
             }
 
             // bottom right
             Button(
-                shape = RoundedCornerShape(percent = 50),
+                shape = RoundedCornerShape(percent = 20),
                 modifier = Modifier
-                    .align(Alignment.BottomEnd)
-                    .fillMaxWidth(0.45f),
+                    .fillMaxHeight(0.48f)
+                    .fillMaxWidth(0.48f)
+                    .align(Alignment.BottomEnd),
                 onClick = {
                     onAnswerClicked(currentQuestion[3].isAnswer)
                 }
             ) {
-                Text(text = currentQuestion[3].name)
+                Text(
+                    text = currentQuestion[3].name,
+                    textAlign = TextAlign.Center
+                )
             }
         }
+
+        Spacer(modifier = Modifier.weight(0.17f))
 
         LinearProgressIndicator(
             progress = progression,
             modifier = Modifier
-                .constrainAs(progressBar) {
-                    top.linkTo(progressBarGuideline)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
+                .align(Alignment.CenterHorizontally)
         )
 
         Text(
             text = "$currentQuestionNumber / $totalQuestionSets",
             modifier = Modifier
-                .constrainAs(progressText) {
-                    top.linkTo(progressBar.bottom, margin = 10.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
+                .align(Alignment.CenterHorizontally)
+                .padding(10.dp)
         )
+
+        Spacer(modifier = Modifier.weight(0.03f))
     }
 }
 
