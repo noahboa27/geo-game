981b09f3e4756d6ef18180bac62f0efaf8abe4d5
noahboa27 <noah.whited@protonmail.com>
2025-02-04 21:10:53 -0700

    figured it was easier to just keep track of one answer country than and answer index

--- app/src/main/java/com/example/geogame/flag_game/domain/data/FlagGameCountry.kt
+++ app/src/main/java/com/example/geogame/flag_game/domain/data/FlagGameCountry.kt
@@ -1,7 +1,7 @@
 package com.example.geogame.flag_game.domain.data
 
 data class FlagGameCountry(
-    val flagUrl: String,   // url used to get the flag image using Coil
-    val name: String,
-    var isAnswer: Boolean
+    val flagUrl: String = "",   // url used to get the flag image using Coil
+    val name: String = "",
+    var isAnswer: Boolean = false
 )
